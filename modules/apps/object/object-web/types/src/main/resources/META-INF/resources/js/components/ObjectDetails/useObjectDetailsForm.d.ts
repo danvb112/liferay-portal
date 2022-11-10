2c3f43cc46ebe60e44cc2e977fb93728473b203b
Daniel Bonasser <danielbonasser@gmail.com>
2022-11-10 15:19:42 -0300

    LPS-162430 Create useObjectDetailsForm hook

---
+++ modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ObjectDetails/useObjectDetailsForm.d.ts
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {FormError} from '@liferay/object-js-components-web';
+interface IUseObjectDetailsForm {
+	initialValues: Partial<ObjectDefinition>;
+	onSubmit: (field: ObjectDefinition) => void;
+}
+export declare type ObjectDetailsErrors = FormError<Partial<ObjectDefinition>>;
+export declare function useObjectDetailsForm({
+	initialValues,
+	onSubmit,
+}: IUseObjectDetailsForm): {
+	errors: FormError<ObjectDefinition>;
+	handleChange: import('react').ChangeEventHandler<HTMLInputElement>;
+	handleSubmit: import('react').FormEventHandler<HTMLFormElement>;
+	setValues: (values: Partial<ObjectDefinition>) => void;
+	values: Partial<ObjectDefinition>;
+};
+export {};
