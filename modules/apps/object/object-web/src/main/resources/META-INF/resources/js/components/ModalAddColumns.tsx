beac46430bbcf334c7633b312acfda17a1106a6f
Daniel Bonasser <danielbonasser@gmail.com>
2022-11-28 18:03:53 -0300

    WIP2 - downloadGIVI

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModalAddColumns.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModalAddColumns.tsx
@@ -28,8 +28,8 @@ function ModalAddColumns<T extends ModalItem>() {
 		{
 			disableRequired,
 			disableRequiredChecked,
-			getName,
 			getLabel,
+			getName,
 			header,
 			items,
 			onSave,
@@ -163,7 +163,7 @@ function ModalAddColumns<T extends ModalItem>() {
 								item.required &&
 								!disableRequiredChecked
 							}
-							label={getLabel?.(item)}
+							label={getLabel?.(item) ?? getName?.(item)}
 							onChange={() => {
 								toggleFieldCheckbox(item.id, !item.checked);
 							}}
@@ -216,8 +216,8 @@ interface ModalItem {
 interface IState<T extends ModalItem> {
 	disableRequired?: boolean;
 	disableRequiredChecked?: boolean;
-	getName?: (name: T) => string;
 	getLabel?: (label: T) => string;
+	getName?: (name: T) => string;
 	header?: string;
 	items: T[];
 	onSave?: (selected: T[]) => void;
