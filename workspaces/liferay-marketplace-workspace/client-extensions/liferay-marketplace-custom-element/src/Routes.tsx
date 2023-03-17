53e32dc392454bf295c1d5375058e6d9a53668e8
Aline Cantarelli <aline.cantarelli@liferay.com>
2023-03-17 13:47:41 -0300

    LPS-178343 Create buttons and forms of Payment methods on Modal

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/Routes.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/Routes.tsx
@@ -4,6 +4,7 @@ import {PublishedAppsDashboardPage} from './pages/PublishedAppsDashboardPage/Pub
 interface AppRoutesProps {
 	route: string;
 }
+
 export default function AppRoutes({route}: AppRoutesProps) {
 	if (route === 'create-new-app') {
 		return <AppCreationFlow />;
