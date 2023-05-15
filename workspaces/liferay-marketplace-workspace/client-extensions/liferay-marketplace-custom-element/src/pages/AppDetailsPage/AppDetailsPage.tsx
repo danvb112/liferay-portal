5beac7c46bebf1dbabf12e7e974073f28ff05961
Daniel Bonasser <danielbonasser@gmail.com>
2023-05-15 18:21:43 -0300

    LPS-183886 Refactor ReviewAndSubmitAppPage component to get new values

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/AppDetailsPage/AppDetailsPage.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/AppDetailsPage/AppDetailsPage.tsx
@@ -183,6 +183,8 @@ export function AppDetailsPage({
 				</ClayNavigationBar>
 
 				<ReviewAndSubmitAppPage
+					productERC={selectedApp.externalReferenceCode}
+					productId={selectedApp.productId}
 					onClickBack={() => {}}
 					onClickContinue={() => {}}
 					productERC={selectedApp.externalReferenceCode}
