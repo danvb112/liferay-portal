b87e82aeb32d2737ade88b9c5a42e97d1c242a2b
Daniel Bonasser <danielbonasser@gmail.com>
2023-09-19 15:30:48 -0300

    LPS-194445 Fix rebase bugs

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/ObjectDefinitionNode/ObjectDefinitionNode.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/ObjectDefinitionNode/ObjectDefinitionNode.tsx
@@ -153,7 +153,7 @@ export function ObjectDefinitionNode({
 				nodes,
 				selectedObjectDefinitionId: id.toString(),
 			},
-			type: TYPES.SET_SELECTED_NODE,
+			type: TYPES.SET_SELECTED_OBJECT_DEFINITION_NODE,
 		});
 	};
 
