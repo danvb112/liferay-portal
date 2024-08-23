2a343f26235efb983e41780d473fd0b264cfbf26
Daniel Bonasser <danielbonasser@gmail.com>
2024-08-23 14:38:21 -0300

    WIP

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/LocalizableText/LocalizableText.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/LocalizableText/LocalizableText.es.js
@@ -35,6 +35,7 @@ const LocalizableText = ({
 	editingLocale = INITIAL_EDITING_LOCALE,
 	fieldName,
 	id,
+	label,
 	name,
 	onFieldBlurred,
 	onFieldChanged = () => {},
@@ -120,6 +121,7 @@ const LocalizableText = ({
 				fieldName={fieldName}
 				id={id}
 				inputValue={inputValue}
+				label={label}
 				name={name}
 				onFieldBlurred={onFieldBlurred}
 				onFieldChanged={(event) => {
@@ -199,6 +201,7 @@ const Main = ({
 	editingLocale,
 	fieldName,
 	id,
+	label,
 	name,
 	onBlur,
 	onChange,
@@ -221,6 +224,7 @@ const Main = ({
 			editingLocale={editingLocale}
 			fieldName={fieldName}
 			id={id}
+			label={label}
 			name={name}
 			onFieldBlurred={onBlur}
 			onFieldChanged={({event, value}) => onChange(event, value)}
